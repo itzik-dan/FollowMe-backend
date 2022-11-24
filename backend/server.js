b36df0b57889841d7172f0a4c2facc3fd7caf333
itzik-dan <itzik0412@gmail.com>
2022-11-24 14:58:53 +0200

    Add index and 404

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 app.use(cors());
+
 require("dotenv").config();
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
@@ -14,23 +15,30 @@ connectDB();
 //Middleware for accepting json data in the req body
 app.use(express.json({ limit: "50mb" }));
 
+// Look inside public folder to look for static files such as css, images etc.
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
 // routes middleware
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 app.use(require("./routes/uploadImage"));
 
+// Catch all the routes that reaches here after fail to match the routes above
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 app.use(errorHandler);
-
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "build", "index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
