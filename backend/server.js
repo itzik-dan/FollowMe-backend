87dca6db443c59aaa356b1b54eeb37aeb2fe80b4
itzik-dan <itzik0412@gmail.com>
2022-11-24 11:48:00 +0200

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+app.use(cors());
 require("dotenv").config();
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
@@ -19,22 +20,19 @@ app.use(require("./routes/uploadImage"));
 
 app.use(errorHandler);
 
-
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static('frontend/build'));
+  // Set static folder
+  app.use(express.static("frontend/build"));
 
-	app.get("*", (req, res) => {
-		res.sendFile(
-			path.resolve("frontend", "build", "index.html")
-		);
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("frontend", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
 );
